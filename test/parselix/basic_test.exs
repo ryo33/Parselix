cd863aa3e32b011ce8c64bb38b2c77838df19fa8
ryo33 <ryo12redstone@gmail.com>
2015-06-14 01:23:38 +0900

    Refactor tests

--- test/parselix/basic_test.exs
+++ test/parselix/basic_test.exs
@@ -3,20 +3,14 @@ defmodule BasicTest do
   use Parselix
 
   test "token" do
-    {result, %AST{label: label, position: position, tree: tree}, remainder, %Position{index: index, vertical: vertical, horizontal: horizontal}} = (parser_token("abc")).("abcdef", %Position{})
-    assert result == :ok
-    assert label == "token"
-    assert tree == "abc"
-    assert position == %Position{}
-    assert remainder == "def"
-    assert index == 3
-    assert vertical == 0
-    assert horizontal == 3
+    assert parser_token("abc").("abcdef", %Position{})
+    == {:ok, %AST{label: "token", position: %Position{}, children: "abc"}, "def", %Position{index: 3, vertical: 0, horizontal: 3}}
   end
 
   test "choice" do
-    assert {:ok, %AST{label: "choice", position: %Position{}, tree: %AST{label: "token", position: %Position{}, tree: "abc"}}, "def", %Position{index: 3, vertical: 0, horizontal: 3}}
-    == (parser_choice([parser_token("abcdefg"), parser_token("bcd"), parser_token("abc")])).("abcdef", %Position{})
+    assert (parser_choice([parser_token("abcdefg"), parser_token("bcd"), parser_token("abc")])).("abcdef", %Position{})
+    == {:ok, %AST{label: "choice", position: %Position{}, children:
+        %AST{label: "token", position: %Position{}, children: "abc"}}, "def", %Position{index: 3, vertical: 0, horizontal: 3}}
   end
 
   test "option" do
--- test/parselix_test.exs
+++ test/parselix_test.exs
@@ -3,8 +3,10 @@ defmodule ParselTest do
   use Parselix
 
   test "get_position" do
-    assert get_position(%Position{index: 1, vertical: 2, horizontal: 300}, "a\nbc\ndef\nghig", "ef\nghig") === %Position{index: 7, vertical: 4, horizontal: 1}
-    assert get_position(%Position{index: 1, vertical: 2, horizontal: 3}, "a\nbc\ndef\nghig", "ef\nghig") === %Position{index: 7, vertical: 4, horizontal: 1}
+    assert get_position(%Position{index: 1, vertical: 2, horizontal: 300}, "a\nbc\ndef\nghig", "ef\nghig")
+    == %Position{index: 7, vertical: 4, horizontal: 1}
+    assert get_position(%Position{index: 1, vertical: 2, horizontal: 3}, "a\nbc\ndef\nghig", "ef\nghig")
+    == %Position{index: 7, vertical: 4, horizontal: 1}
   end
 
   test "unfold" do
