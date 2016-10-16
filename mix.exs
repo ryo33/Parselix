e870f47741c560e7b0d23282f45b340080de8c0a
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-10-16 18:54:15 +0900

    Update to elixir 1.3

--- mix.exs
+++ mix.exs
@@ -14,30 +14,17 @@ defmodule Parselix.Mixfile do
          "GitHub" => "https://github.com/ryo33/Parselix",
        }
      ],
-     elixir: "~> 1.0",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    [{:earmark, "~> 0.1.17", only: :dev},
-     {:ex_doc, "~> 0.8.1", only: :dev}]
+    [{:ex_doc, "~> 0.14", only: :dev}]
   end
 end
